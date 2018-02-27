6e52d226e194b0b372eb013d42709ae4f04fc4f7
isanbel <theisanbel@email.com>
2018-02-27 15:51:31 +0800

    pass data through segue

---
+++ MyChat/Controller/ChatPageViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ChatPageViewController.swift
+//  MyChat
+//
+//  Created by Yuanyuan Zhang on 27/02/2018.
+//  Copyright © 2018 Ithink Team. All rights reserved.
+//
+
+import UIKit
+
+class ChatPageViewController: UIViewController {
+    
+    @IBOutlet var friendAvatarView: UIImageView!
+    
+    var friendAvatar = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        friendAvatarView.image = UIImage(named: friendAvatar)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyChat/Controller/ChatTableViewController.swift
+++ MyChat/Controller/ChatTableViewController.swift
@@ -131,14 +131,16 @@ class ChatTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "showChatPage" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationViewController = segue.destination as! ChatPageViewController
+                destinationViewController.friendAvatar = chatOutlines[indexPath.row].avatar
+            }
+        }
     }
-    */
 
 }
