9a8bfe65712028e4e8ad4bfe7f83cbf5ec818a48
painterdrown <painterdrown@hotmail.com>
2018-03-08 16:40:43 +0800

    HttpUtil 上传图片

--- MyChat/Resource/HttpUtil.swift
+++ MyChat/Resource/HttpUtil.swift
@@ -15,8 +15,7 @@ class HttpUtil {
     
     static func post(url: String, parameters: Parameters, onSuccess: @escaping([String: Any]) -> Void, onFailure: @escaping([String: Any]) -> Void) {
         Alamofire.request(BASE_URL + url, method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
-            switch response.result.isSuccess {
-            case true:
+            if (response.result.isSuccess) {
                 let value = response.result.value!
                 let json = JSON(value)
                 print(json)
@@ -25,9 +24,48 @@ class HttpUtil {
                 } else {
                     onFailure(json.dictionaryObject!)
                 }
-            case false:
+            } else {
                 print(response.result.error!)
             }
         }
+    }
+    
+    static func get(url: String, onSuccess: @escaping([String: Any]) -> Void, onFailure: @escaping([String: Any]) -> Void) {
+        Alamofire.request(BASE_URL + url).responseJSON { response in
+            if (response.result.isSuccess) {
+                let value = response.result.value!
+                let json = JSON(value)
+                print(json)
+                if let code = json["code"].int, code == 0 {
+                    onSuccess(json.dictionaryObject!)
+                } else {
+                    onFailure(json.dictionaryObject!)
+                }
+            } else {
+                print(response.result.error!)
+            }
+        }
+    }
+    
+    static func postWithImage(url: String, image: UIImage, onSuccess: @escaping([String: Any]) -> Void, onFailure: @escaping([String: Any]) -> Void) {
+        Alamofire.upload(
+            multipartFormData: { multipartFormData in
+                let image_data = UIImageJPEGRepresentation(image, 0.5)
+                multipartFormData.append(image_data!, withName: "image", fileName: "image.jpg", mimeType: "image/jpg")
+            },
+            usingThreshold: UInt64.init(),
+            to: BASE_URL + url,
+            method: .post,
+            headers: nil,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON { response in
+                        debugPrint(response)
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+            })
     }
 }
