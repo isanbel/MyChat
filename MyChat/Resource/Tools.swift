8e7c5b2d31149fc920d82296201539b31e59e9bb
isanbel <theisanbel@email.com>
2018-03-13 21:24:12 +0800

    update relative time

--- MyChat/Resource/Tools.swift
+++ MyChat/Resource/Tools.swift
@@ -10,22 +10,29 @@ import Foundation
 
 // Reference: https://stackoverflow.com/questions/27310883/swift-ios-doesrelativedateformatting-have-different-values-besides-today-and
 extension Date {
-    var yearsFromNow:   Int { return Calendar.current.dateComponents([.year],       from: self, to: Date()).year        ?? 0 }
-    var monthsFromNow:  Int { return Calendar.current.dateComponents([.month],      from: self, to: Date()).month       ?? 0 }
-    var weeksFromNow:   Int { return Calendar.current.dateComponents([.weekOfYear], from: self, to: Date()).weekOfYear  ?? 0 }
-    var daysFromNow:    Int { return Calendar.current.dateComponents([.day],        from: self, to: Date()).day         ?? 0 }
-    var hoursFromNow:   Int { return Calendar.current.dateComponents([.hour],       from: self, to: Date()).hour        ?? 0 }
-    var minutesFromNow: Int { return Calendar.current.dateComponents([.minute],     from: self, to: Date()).minute      ?? 0 }
-    var secondsFromNow: Int { return Calendar.current.dateComponents([.second],     from: self, to: Date()).second      ?? 0 }
+    var daysFromNow: Int { return Calendar.current.dateComponents([.day], from: self, to: Date()).day         ?? 0 }
+    var hoursFromNow: Int { return Calendar.current.dateComponents([.hour], from: self, to: Date()).hour ?? 0 }
+    var minutesFromNow: Int { return Calendar.current.dateComponents([.minute], from: self, to: Date()).minute ?? 0 }
+    var secondsFromNow: Int { return Calendar.current.dateComponents([.second], from: self, to: Date()).second ?? 0 }
     var relativeTime: String {
-        if yearsFromNow   > 0 { return "\(yearsFromNow) 年前" }
-        if monthsFromNow  > 0 { return "\(monthsFromNow) 月前" }
-        if weeksFromNow   > 0 { return "\(weeksFromNow) 周前" }
-        if daysFromNow    > 0 { return daysFromNow == 1 ? "昨天" : "\(daysFromNow) 天前" }
-        if hoursFromNow   > 0 { return "\(hoursFromNow) 小时前" }
-        if minutesFromNow > 0 { return "\(minutesFromNow) 分钟前" }
-        if secondsFromNow >= 0 { return secondsFromNow < 15 ? "刚刚"
-            : "\(secondsFromNow) 秒前" }
-        return ""
+        let formatter = DateFormatter()
+        // 日期
+        formatter.dateFormat = "dd/MM/yyyy"
+        var date = formatter.string(from: self)
+        // 时间
+        formatter.dateFormat = "hh:mm"
+        var time = formatter.string(from: self)
+        if daysFromNow == 0 { date = "今天" }
+        if daysFromNow == 1 { date = "昨天" }
+        if daysFromNow == 2 { date = "前天" }
+        if hoursFromNow == 0 && secondsFromNow >= 0 {
+            date = ""
+            if minutesFromNow > 0 {
+                time = "\(minutesFromNow) 分钟前"
+            } else {
+                time = secondsFromNow < 15 ? "刚刚" : "\(secondsFromNow) 秒前"
+            }
+        }
+        return date == "" ? time : date + " " + time
     }
 }
