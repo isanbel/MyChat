bae94cdd1dee9c60469525bfdd7ad7b5164bbf47
isanbel <theisanbel@email.com>
2018-04-20 16:17:39 +0800

    remove duplication

--- MyChat/Controller/ChatTableViewController+UnreadMessages.swift
+++ MyChat/Controller/ChatTableViewController+UnreadMessages.swift
@@ -25,45 +25,4 @@ extension ChatTableViewController: UnreadMessageDelegate {
         // update Tabbar
         ChatTableViewController.noticeDelegate?.updateUnreadMessages()
     }
-    
-    func saveUnreadMessages(messages: [String], from: String) {
-        var friend = FriendMO()
-        for friendTemp in Global.user.friends!.array as! [FriendMO] {
-            if friendTemp.id == from {
-                friend = friendTemp
-                break
-            }
-        }
-        
-        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
-            for msg in messages {
-                Utils.appendDateIndicatorIfNeeded(whenChattingWith: friend)
-                
-                let message = ChatMessageMO(context: appDelegate.persistentContainer.viewContext)
-                message.isSent = false
-                message.date = Date()
-                message.contentText = msg
-                message.friend = friend
-                message.isDateIdentifier = false
-                
-                // delete the old last message and add the new one
-                var unread  = 0
-                let context = appDelegate.persistentContainer.viewContext
-                if let unreadCount = friend.lastMessage?.unreadCount {
-                    unread = Int(unreadCount)
-                }
-                if friend.lastMessage != nil {
-                    context.delete(friend.lastMessage!)
-                }
-                // update lastMessage
-                let lastmessage = LastMessageMO(context: appDelegate.persistentContainer.viewContext)
-                lastmessage.content = msg
-                lastmessage.date = Date()
-                lastmessage.unreadCount = Int32(1 + unread)
-                unread = Int(lastmessage.unreadCount)
-                friend.lastMessage = lastmessage
-            }
-            appDelegate.saveContext()
-        }
-    }
 }
